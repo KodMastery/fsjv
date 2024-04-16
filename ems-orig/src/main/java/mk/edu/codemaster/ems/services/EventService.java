0758c8c1d79b84f0d45c0d32c70c04ffea743b3d
Aleksandar <aleksandargorgiev@hotmail.com>
2024-04-16 23:58:11 +0200

    Made changes to EventService

--- ems-orig/src/main/java/mk/edu/codemaster/ems/services/EventService.java
+++ ems-orig/src/main/java/mk/edu/codemaster/ems/services/EventService.java
@@ -1,12 +1,35 @@
 package mk.edu.codemaster.ems.services;
 
 import mk.edu.codemaster.ems.repositories.EventRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import mk.edu.codemaster.ems.entities.Event;
+
 
 @Service
 public class EventService {
 
     @Autowired private EventRepository eventRepository;
+    
+    public List<Event> getAllEvents() {
+      return eventRepository.findAll();
+  }
 
+  public Event getEventById(Long id) {
+      return eventRepository.findById(id).orElse(null);
+  }
+
+  public Event createEvent(Event event) {
+      // Add any validation or additional logic here if needed
+      return eventRepository.save(event);
+  }
+
+  public void deleteEvent(Long id) {
+      eventRepository.deleteById(id);
+  }
 }
+
+
